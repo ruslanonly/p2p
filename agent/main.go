bcdf8f102674ef901cb3d8ab0b3eb29da668a3de
ruslanonly <rzaevruslanonly@gmail.com>
2025-05-11 15:22:19 +0300

    Add peers limit env variable handling

--- agent/main.go
+++ agent/main.go
@@ -11,20 +11,18 @@ import (
 func main() {
 	port := 5000
 
+	maxPeers := os.Getenv("PEERS_LIMIT")
+	if maxPeers == "" {
+		log.Fatalf("Параметр PEERS_LIMIT не указан")
+	}
+
 	agent, err := agentPkg.NewAgent(context.Background(), 3, port)
 	if err != nil {
 		log.Fatalf("Ошибка при инициализации узла: %v", err)
 	}
 
 	bootstrapIP := os.Getenv("BOOTSTRAP_IP")
-	if err != nil {
-		log.Fatalf("Параметр BOOTSTRAP_PORT указан неверно: %v", err)
-	}
-
 	bootstrapPeerID := os.Getenv("BOOTSTRAP_PEER_ID")
-	if err != nil {
-		log.Fatalf("Параметр BOOTSTRAP_PEER_ID указан неверно: %v", err)
-	}
 
 	if bootstrapIP != "" && bootstrapPeerID != "" {
 		log.Printf("Bootstrap IP: %s", bootstrapIP)
